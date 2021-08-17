5d7caaff3582c0d12540f5695c1a1a69a3db790b
Prykhodko-Pasha <prykhodko.p@gmail.com>
2021-08-17 21:33:33 +0300

    almost

--- src/js/index.js
+++ src/js/index.js
@@ -1,79 +1,103 @@
-import countryTpl from '../templates/country.hbs';
-import fetchCountries from './fetchCountries';
+import imageTpl from '../templates/image.hbs';
+import fetchImages from './apiService';
 import debounce from 'lodash.debounce';
 
 const refs = {
-  input: document.querySelector('#input'),
-  output: document.querySelector('.country'),
+  input: document.querySelector('#search-form'),
+  output: document.querySelector('.gallery'),
+  loadMoreBtn: document.querySelector('#loadMore'),
 };
 
+let pageNumber = 1;
+let searchQuery = '';
+
+// noty
+import Noty from 'noty';
+import 'noty/src/noty.scss';
+import 'noty/src/themes/nest.scss';
+
+// basicLightbox
+import * as basicLightbox from 'basiclightbox';
+
 // pnotify
-import '@pnotify/core/dist/PNotify.css';
-import '@pnotify/core/dist/BrightTheme.css';
-import { error } from '@pnotify/core';
-import { defaults } from '@pnotify/core';
-defaults.maxTextHeight = null; //Deleting Maximum height of the text container
+// import '@pnotify/core/dist/PNotify.css';
+// import '@pnotify/core/dist/BrightTheme.css';
+// import { error } from '@pnotify/core';
+// import { defaults } from '@pnotify/core';
+// defaults.maxTextHeight = null; //Deleting Maximum height of the text container
 //===========
 
-refs.input.addEventListener('input', debounce(searchCountry, 500));
+refs.input.addEventListener('input', debounce(searchImages, 500));
+refs.loadMoreBtn.addEventListener('click', onLoadMoreBtn);
+refs.output.addEventListener('click', e => {
+  if (e.target.className === 'card-img') {
+    onOpenLightbox(e.target.dataset.source);
+  }
+});
 
-function searchCountry(e) {
-  const searchQuery = e.target.value.trim();
+function searchImages(e) {
+  refs.loadMoreBtn.classList.add('is-hidden');
+  searchQuery = e.target.value.trim();
   // console.log(searchQuery);
   if (searchQuery) {
-    generateSearchQueryResult(searchQuery);
+    pageNumber = 1;
+    generateSearchQueryResult(searchQuery, pageNumber);
   }
   resetOutput();
 }
 
-function generateSearchQueryResult(searchQuery) {
-  fetchCountries(searchQuery)
+function onLoadMoreBtn() {
+  pageNumber += 1;
+  console.log(pageNumber);
+  console.log(searchQuery);
+  generateSearchQueryResult(searchQuery, pageNumber);
+}
+
+function generateSearchQueryResult(searchQuery, pageNumber) {
+  fetchImages(searchQuery, pageNumber)
     .then(data => {
       // console.log(data);
-      if (data.length > 10) {
-        resetOutput();
-        error({
-          title: 'Sorry!',
-          text: 'Too many matches found. Please enter a more specific query!',
-          delay: 3000,
-        });
-      } else if (data.length === 1) {
-        const countryMarkup = countryTpl(data[0]);
-        refs.output.innerHTML = countryMarkup;
-      } else if (data.length <= 10) {
-        const countryMarkup = data
-          .map(country => `<li><a href=# class="country__item">${country.name}</a></li>`)
-          .join('');
-        refs.output.innerHTML = `<ul class="country__list">${countryMarkup}</ul>`;
-        const countryListEl = document.querySelector('.country__list');
-        countryListEl.addEventListener('click', onCountryClick);
+      if (data.hits.length === 0) {
+        refs.loadMoreBtn.classList.add('is-hidden');
+
+        new Noty({
+          theme: 'nest',
+          type: 'error',
+          text: 'Sorry!<br>No matches found...',
+          timeout: 3000,
+        }).show();
       } else {
-        resetOutput();
-        error({
-          title: 'Sorry!',
-          text: 'No matches found. Please enter a correct query!',
-          delay: 3000,
+        const galleryMarkup = data.hits.map(image => imageTpl(image)).join('');
+        refs.output.insertAdjacentHTML('beforeend', galleryMarkup);
+
+        refs.loadMoreBtn.classList.remove('is-hidden');
+
+        refs.loadMoreBtn.scrollIntoView({
+          behavior: 'smooth',
+          block: 'end',
         });
+
+        const totalPages = Math.ceil(data.total / 12);
+        if (totalPages === pageNumber) {
+          refs.loadMoreBtn.classList.add('is-hidden');
+        }
       }
     })
     .catch(err => {
       console.log("I've cathed error: ", err);
-      error({
-        title: 'Oops!',
-        text: 'Something went wrong on your side...',
-        delay: 3000,
-      });
+      new Noty({
+        theme: 'nest',
+        type: 'error',
+        text: "Sorry!<br>Something went wrong...<br>Can't load more images<br>:(",
+        timeout: 5000,
+      }).show();
     });
+}
+
+function onOpenLightbox(src) {
+  basicLightbox.create(`<img src=${src}>`).show();
 }
 
 function resetOutput() {
   refs.output.innerHTML = '';
-}
-
-function onCountryClick(e) {
-  if (e.target.className === 'country__item') {
-    const searchQuery = e.target.textContent;
-    // console.log(searchQuery);
-    generateSearchQueryResult(searchQuery);
-  }
 }
